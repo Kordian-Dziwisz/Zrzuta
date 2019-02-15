67634b1c3c56b555a1c7a9ced215e06343e5880c
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-02-15 14:48:41 +0100

    Fundraising list ver. alpha + minor fixes in App.vue and NavBar.vue

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="main">
-        <login @login='changeLogin' v-if="!logged" />
-        <p>{{login}}</p>
-        <!-- <List /> -->
-    </div>
+  <div class="main">
+    <login @login="changeLogin" v-if="!logged"/>
+    <p>{{login}}</p>
+    <!-- <List /> -->
+    <fundraising-list v-if="logged"></fundraising-list>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import Login from '@/components/Login.vue'
-
+import Vue from "vue";
+import Login from "@/components/Login.vue";
+import FundraisingList from "@/components/FundraisingList.vue";
 // import List from '@/components/List.vue'
 
 export default {
-    data(){
-        return{
-            logged: false,
-            login: '',
-            placeholder: 12
-        }
-    },
-    methods: {
-        changeLogin(login){
-            this.logged = true;
-            localStorage.setItem('login', login);
-            this.login = localStorage.getItem('login');
-        }
-    },
-    components: {
-        Login
-        //List
+  data() {
+    return {
+      logged: false,
+      login: "",
+      placeholder: 12
+    };
+  },
+  methods: {
+    changeLogin(login) {
+      this.logged = true;
+      localStorage.setItem("login", login);
+      this.login = localStorage.getItem("login");
     }
-}
+  },
+  components: {
+    Login,
+    FundraisingList
+    //List
+  }
+};
 </script>
 
