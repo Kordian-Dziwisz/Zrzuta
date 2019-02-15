0a9a8fc748d7cf287cb8e0bc454e628781c36635
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-02-15 13:30:21 +0100

    Repair Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -23,16 +23,5 @@ export default {
 }
 </script>
 <style scoped>
-    .input {
-        position: static;
-        margin: 25vh 25vw;
-        width: 50vw;
-        height: 20vh;
-        padding-top: 12vh;
-        text-align: center;
-        border: 2px solid #ccc;
-        border-radius: 1vh;
-        box-shadow: .5vw .5vw 1vh rgba(00, 00, 00, 50%);
-    }
 </style>
 
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -28,7 +28,7 @@ export default {
         }
     },
     components: {
-        Input
+        Login
         //List
     }
 }
