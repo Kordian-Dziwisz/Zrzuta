1e3911fc1e26e0498bc28a00b8808bd58f6cd17f
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-02-16 09:03:11 +0100

    Added better placeholders to fundraises

--- src/components/FundraisingList.vue
+++ src/components/FundraisingList.vue
@@ -7,7 +7,9 @@
       <div v-for="infokey in listInfo" :key="infokey">
         <p>{{infokey.description}}</p>
         <p>Utworzył: {{infokey.madeBy}}</p>
-        <p>Stan: {{infokey.paymentStatus}}</p>
+        <p>
+          <strong>Stan: {{infokey.paymentStatus}}</strong>
+        </p>
       </div>
     </div>
   </div>
@@ -23,12 +25,13 @@ export default {
           description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu leo velit. Etiam sollicitudin arcu turpis, sed placerat mi tincidunt a. Vivamus in lorem pulvinar, ultricies nibh nec, volutpat sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed hendrerit mi non elementum laoreet. Etiam at urna id libero rhoncus semper at quis est. Ut molestie gravida turpis a venenatis. Praesent viverra consectetur ante id efficitur. Sed placerat ex vel neque maximus, at tristique urna egestas. Suspendisse placerat, arcu non tincidunt finibus, augue velit vehicula lacus, id dapibus leo nisi a justo. ",
           madeBy: "Bartek",
-          paymentStatus: "zapłacone"
+          paymentStatus: "zapłaciłeś"
         },
         {
-          description: "coscoscos",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu leo velit. Etiam sollicitudin arcu turpis, sed placerat mi tincidunt a. Vivamus in lorem pulvinar, ultricies nibh nec, volutpat sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed hendrerit mi non elementum laoreet. Etiam at urna id libero rhoncus semper at quis est. Ut molestie gravida turpis a venenatis. Praesent viverra consectetur ante id efficitur. Sed placerat ex vel neque maximus, at tristique urna egestas. Suspendisse placerat, arcu non tincidunt finibus, augue velit vehicula lacus, id dapibus leo nisi a justo. ",
           madeBy: "Adam",
-          paymentStatus: "niezapłacone"
+          paymentStatus: "nie zapłaciłeś"
         }
       ]
     };
