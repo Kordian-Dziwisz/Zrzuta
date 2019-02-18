c3f4a54a4a06a5fbd2788fd17018aa96bf2a84e6
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-02-18 14:39:08 +0100

    naprawa błędów

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="main">
-    <login @login="changeLogin" v-if="!logged"/>
-    <fundraising-list v-if="logged" :list="list"></fundraising-list>
-  </div>
+    <div class="main">
+        <login @login="changeLogin" v-if="!logged"/>
+        <fundraising-list v-if="logged" :list="list"></fundraising-list>
+    </div>
 </template>
 
 <script>
@@ -13,55 +13,53 @@ import firebase from "firebase";
 // import List from '@/components/List.vue'
 
 export default {
-  data() {
-    return {
-      logged: false,
-      login: "",
-      db: firebase.firestore().collection("Zrzuty"),
-      list: [
-        {
-          docID: "uRLJNTchecAfASfPUKT5",
-          creator: "Kordian",
-          title: "title",
-          description: "Poor project",
-          creationDate: new Date("December 17, 1995 03:24:00"),
-          endDate: new Date("December 17, 1995 03:24:00"),
-          ended: false
+    data() {
+        return {
+            logged: false,
+            login: "",
+            db: firebase.firestore().collection("Zrzuty"),
+            list: [
+                {
+                    docID: "uRLJNTchecAfASfPUKT5",
+                    creator: "Kordian",
+                    title: "title",
+                    description: "Poor project",
+                    creationDate: new Date("December 17, 1995 03:24:00"),
+                    endDate: new Date("December 17, 1995 03:24:00"),
+                    ended: false
+                }
+            ]
+        };
+    },
+    mounted() {
+        this.login = localStorage.getItem("login");
+        this.getList();
+    },
+    methods: {
+        changeLogin(login) {
+            if (login != "") {
+                this.logged = true;
+            }
+            localStorage.setItem("login", login);
+        },
+        async getList() {
+            (await this.db.get()).docs.map(item => this.mapItem(item));
+        },
+        mapItem(item) {
+            console.log(item.data().fundraisInfo);
+            console.log(item.id);
+            return { ...item.data().fundraisInfo, ...{ id: item.id } };
         }
-      ]
-    };
-  },
-  mounted() {
-    this.login = localStorage.getItem("login");
-    this.getList();
-  },
-  methods: {
-    changeLogin(login) {
-      if (login != "") {
-        this.logged = true;
-      }
-      localStorage.setItem("login", login);
     },
-    async getList() {
-      (await this.db.get()).docs.map(item => {
-        this.mapItem(item);
-      });
-    },
-    mapItem(item) {
-      console.log(item.data().fundraisInfo);
-      console.log(item.id);
-      return { ...item.data().fundraisInfo, ...{ id: item.id } };
+    components: {
+        Login,
+        FundraisingList
     }
-  },
-  components: {
-    Login,
-    FundraisingList
-  }
 };
 </script>
 <style scoped>
 .main {
-  padding-top: 80px;
+    padding-top: 80px;
 }
 </style>
 
