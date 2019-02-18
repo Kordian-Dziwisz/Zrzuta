21e4556013993956441ace829e8b416b2f8315c9
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-02-18 14:57:23 +0100

    Naprawiony Navbar, teraz prowadzi z pomocą params: {id},  przenosi nas do danej podstrony

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <b-navbar class="fixed-top" toggleable="md">
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <div id="app">
+        <b-navbar class="fixed-top" toggleable="md">
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-brand href="#" id="paddingCenter">
-        <img src="@/assets/Logo.jpg" width="50">
-      </b-navbar-brand>
-      <!--https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfuNsZHffYNmIn1UcFABUb6-GnknWFFrXIniz1R-Ocer4BMCMug -->
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-          <b-nav-item :to="{ name: 'Main'}">Home</b-nav-item>
-          <b-nav-item :to="{ name: 'FundraisDetails'}">Nowa zbiórka</b-nav-item>
-        </b-navbar-nav>
+            <b-navbar-brand href="#" id="paddingCenter">
+                <img src="@/assets/Logo.jpg" width="50">
+            </b-navbar-brand>
+            <!--https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfuNsZHffYNmIn1UcFABUb6-GnknWFFrXIniz1R-Ocer4BMCMug -->
+            <b-collapse is-nav id="nav_collapse">
+                <b-navbar-nav>
+                    <b-nav-item :to="{ name: 'Main'}">Home</b-nav-item>
+                    <b-nav-item :to="{ name: 'FundraisDetails', params: {id: 12345}}">Nowa zbiórka</b-nav-item>
+                </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-navbar-brand right>
-            <p id="marginCenter">Oddajcie mi hajs</p>
-          </b-navbar-brand>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-navbar-brand right>
+                        <p id="marginCenter">Oddajcie mi hajs</p>
+                    </b-navbar-brand>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
 </template>
 <script>
 export default {};
@@ -29,17 +29,17 @@ export default {};
 
 <style scoped>
 .navbar {
-  background-color: white;
-  box-shadow: 0.5vw 0.5vw 1vh rgba(00, 00, 00, 50%);
-  border: 1px solid #ccc;
-  border-radius: 0.25rem;
+    background-color: white;
+    box-shadow: 0.5vw 0.5vw 1vh rgba(00, 00, 00, 50%);
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
 }
 
 #paddingCenter {
-  padding-top: 0;
+    padding-top: 0;
 }
 
 #marginCenter {
-  margin: 0;
+    margin: 0;
 }
 </style>
