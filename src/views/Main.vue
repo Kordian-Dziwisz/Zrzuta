e65356420665786365d4c1f50d98ed9ba2bddd8d
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-02-18 15:00:51 +0100

    merge z main 2

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,8 @@
 <template>
-<<<<<<< HEAD
   <div class="main">
     <login class="loginVisibility" @login="changeLogin" v-if="!logged"/>
     <fundraising-list v-if="logged" :list="list"></fundraising-list>
   </div>
-=======
-    <div class="main">
-        <login @login="changeLogin" v-if="!logged"/>
-        <fundraising-list v-if="logged" :list="list"></fundraising-list>
-    </div>
->>>>>>> a939d686c79f811d53a1d8731d843c6e597574e8
 </template>
 
 <script>
@@ -20,54 +13,52 @@ import firebase from "firebase";
 // import List from '@/components/List.vue'
 
 export default {
-    data() {
-        return {
-            logged: false,
-            login: "",
-            db: firebase.firestore().collection("Zrzuty"),
-            list: [
-                {
-                    docID: "uRLJNTchecAfASfPUKT5",
-                    creator: "Kordian",
-                    title: "title",
-                    description: "Poor project",
-                    creationDate: new Date("December 17, 1995 03:24:00"),
-                    endDate: new Date("December 17, 1995 03:24:00"),
-                    ended: false
-                }
-            ]
-        };
-    },
-    mounted() {
-        this.login = localStorage.getItem("login");
-        this.getList();
-    },
-    methods: {
-        changeLogin(login) {
-            if (login != "") {
-                this.logged = true;
-            }
-            localStorage.setItem("login", login);
-        },
-        async getList() {
-            this.list = (await this.db.get()).docs.map(item =>
-                this.mapItem(item)
-            );
-            console.log(this.list);
-        },
-        mapItem(item) {
-            return { ...item.data().fundraisInfo, id: item.id };
+  data() {
+    return {
+      logged: false,
+      login: "",
+      db: firebase.firestore().collection("Zrzuty"),
+      list: [
+        {
+          docID: "uRLJNTchecAfASfPUKT5",
+          creator: "Kordian",
+          title: "title",
+          description: "Poor project",
+          creationDate: new Date("December 17, 1995 03:24:00"),
+          endDate: new Date("December 17, 1995 03:24:00"),
+          ended: false
         }
+      ]
+    };
+  },
+  mounted() {
+    this.login = localStorage.getItem("login");
+    this.getList();
+  },
+  methods: {
+    changeLogin(login) {
+      if (login != "") {
+        this.logged = true;
+      }
+      localStorage.setItem("login", login);
     },
-    components: {
-        Login,
-        FundraisingList
+    async getList() {
+      this.list = (await this.db.get()).docs.map(item => this.mapItem(item));
+      console.log(this.list);
+    },
+    mapItem(item) {
+      return { ...item.data().fundraisInfo, id: item.id };
     }
+  },
+  components: {
+    Login,
+    FundraisingList
+  }
 };
 </script>
 <style scoped>
 .main {
-    padding-top: 80px;
+  padding-top: 80px;
 }
 </style>
 
