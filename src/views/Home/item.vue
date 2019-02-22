3c80921c8e87c5ddbcbcdf21b2c8da3b7ca2a9df
Kordian Dziwisz <kordian.dziwisz@gmail.com>
2019-02-22 10:07:21 +0100

    Lista zbiórek teraz ma w miarę poprawnie ułożone elementy

--- src/views/Home/item.vue
+++ src/views/Home/item.vue
@@ -2,40 +2,37 @@
   <b-container class="w-100 m-0 text-dark bg-light" fluid>
     <b-row class="mx-auto border-bottom-1 mt-3">
       <b-col md="0">{{item.index}}</b-col>
+
       <b-col sm="5">
-        <p class="h4 d-inline mr-1">{{item.title}}</p>twórca:
+        <p class="h4 mr-1">{{item.title}}</p>twórca:
         <p class="h6 d-inline">{{item.creator}}</p>
       </b-col>
-      <b-col sm="4" class="pt-1">status:
+
+      <b-col sm="4" class="pt-4 px-0">status:
         <p class="h6 d-inline" v-if="item.endDate < new Date(Date.now())">zbiórka już się zakończyła</p>
         <p class="h6 d-inline" v-else-if="item.ended">Dokonaj zapłaty</p>
         <p class="h6 d-inline" v-else>Zbiórka jest w trakcie realizacji</p>
       </b-col>
-      <b-col sm="1">
-        <router-link
-          :to="{name: 'Fundrais', params: {id: item.id}}"
-          @click="removeItem"
-          v-if="isYour"
-        >
-          <b-button class="secondary">Edytuj</b-button>
+
+      <b-col sm="2" class="p-0">
+        <router-link :to="{name: 'Fundrais', params: {id: item.id}}" v-if="isYour">
+          <b-button class="btn-secondary px-1 mr-1">Edytuj</b-button>
         </router-link>
-        <router-link :to="{name: 'Fundrais', params: {id: item.id}}" @click="removeItem" v-else>
-          <b-button class="secondary">Otwórz</b-button>
+        <router-link :to="{name: 'Fundrais', params: {id: item.id}}" v-else>
+          <b-button class="btn-secondary px-0 mr-1">Otwórz</b-button>
         </router-link>
+        <b-button class="btn-danger" @click="removeItem">Usuń</b-button>
       </b-col>
       <hr>
     </b-row>
     <b-row class="pl-5 mx-auto">
-      <b-col md="6">{{item.description}}</b-col>
-      <span class="border-bottom"></span>
+      <b-col md="5" class="overflow-auto">{{item.description}}</b-col>
+
+      <b-col
+        v-if="!item.endDate < new Date(Date.now())"
+        md="5"
+      >Zbiórka kończy się: {{ item.endDate | moment("dddd, d MMMM YYYY")}} o godzinie: {{ item.endDate | moment("h:mm")}}</b-col>
     </b-row>
-    <!-- <router-link
-      :to="{name: 'Fundrais', params: {id: item.id}}"
-      @click="removeItem"
-      v-if="isYour"
-    >Edit</router-link>-->
-    <!-- <router-link :to="{name: 'Fundrais', params: {id: item.id}}" @click="removeItem" v-else>Show</router-link> -->
-    <!-- Zbiórka kończy się: {{ item.endDate | moment("dddd, MMMM, YYYY")}} o godzinie: {{ item.endDate | moment("h:mm")}} -->
   </b-container>
 </template>
 <script>
--- src/views/Home/list.vue
+++ src/views/Home/list.vue
@@ -3,7 +3,7 @@
     <p class="display-4">Lista zbiórek</p>
     <ul class="list-group" v-for="(item, index) in list" :key="index">
       <p v-if="list.length == 0">Brak aktywnych zbiórek</p>
-      <Item :class="list-group-item" :item="{index: index, ...item}" @remove="removeItem"/>
+      <Item class="list-group-item" :item="{index: index, ...item}" @remove="removeItem"/>
     </ul>
   </b-container>
 </template>
