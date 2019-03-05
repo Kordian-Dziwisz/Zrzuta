f967046016ae2a46cc1c4fca839bc932cac4b367
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-03-05 09:12:04 +0100

    Dodanie wyglądu do projectinfo i naprawa navbara

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar
-      class="fixed-top shadow bg-light"
+      class="fixed-top shadow bg-white"
       toggleable="md"
       v-shortkey="['alt', 'n']"
       @shortkey="addNewFundrais"
