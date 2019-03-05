7ebddcda6b81f9c4225aa1bb784be861d1eb24cb
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-03-05 15:23:43 +0100

    Dodanie widoku do item.admin i naprawienie widoku item w uczestnikach

--- src/views/Fundrais/Participants/item.admin.vue
+++ src/views/Fundrais/Participants/item.admin.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="ParticipantsListItem">
-    <span>{{item.index + 1}}: {{item.name}} |</span>
-    <span>{{item.comment}} |</span>
-    <span v-if="item.paid && !item.accepted">Wpłacono |</span>
-    <span v-if="item.accepted">Otrzymano</span>
-    <div class="control">
-      <b-button class="mx-1" @click="accepted" v-if="!item.accepted">Zaakceptuj</b-button>
-      <b-button class="mx-1" @click="remove">Usuń</b-button>
+    <div class="row">
+      <span class="col-lg-4">{{item.index + 1}}: {{item.name}}</span>
+      <span class="col-lg-4">
+        <span v-if="item.paid && !item.accepted">Wpłacono</span>
+        <span v-if="item.accepted">Otrzymano</span>
+      </span>
+      <div class="col-lg-2">
+        <b-button
+          class="mt-1 btn-outline-info btn-light"
+          size="sm"
+          v-if="!item.accepted"
+          @click="accepted"
+        >Zaakceptuj</b-button>
+      </div>
+    </div>
+    <div class="row">
+      <span class="col-lg-8">{{item.comment}}</span>
+      <span class="col-lg-3">
+        <b-button class="my-2 btn-outline-danger btn-light" size="sm" @click="remove">Usuń</b-button>
+      </span>
     </div>
   </div>
 </template>
--- src/views/Fundrais/Participants/item.vue
+++ src/views/Fundrais/Participants/item.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="ParticipantsListItem">
     <div class="row">
-      <span class="col-5">{{item.index + 1}}: {{item.name}}</span>
-      <span class="col-5" v-if="item.paid && !item.accepted && authenticate">Zapłacono</span>
-      <span class="col-5" v-if="item.accepted">Otrzymano</span>
-      <span class="col-5 pl-0 ml-5">
+      <span class="col-lg-4">{{item.index + 1}}: {{item.name}}</span>
+      <span class="col-lg-4">
+        <span v-if="item.paid && !item.accepted && authenticate">Zapłacono</span>
+        <span v-if="item.accepted">Otrzymano</span>
+      </span>
+      <div class="col-lg-2">
         <b-button
-          class="ml-5 mt-1 btn-outline-info btn-light"
+          class="mt-1 btn-outline-info btn-light"
           size="sm"
           v-if="authenticate && !item.paid && !item.accepted"
           @click="paid"
         >Zapłaciłem</b-button>
-      </span>
+      </div>
     </div>
     <div class="row container">
-      <b-textarea class="col-8 mt-2 mb-1" v-model="item.comment" v-if="authenticate"></b-textarea>
+      <b-textarea class="col-lg-9 mt-1 mb-1" v-model="item.comment" v-if="authenticate"></b-textarea>
       <span v-else>{{item.comment}}</span>
-      <span class="col pl-0 ml-1">
+      <span class="col-lg-3">
         <b-button
-          class="ml-4 mt-2 btn-outline-danger btn-light"
+          class="my-2 mt-2 btn-outline-danger btn-light"
           size="sm"
           @click="remove"
           v-if="authenticate"
         >Usuń</b-button>
       </span>
     </div>
-    <div class="control"></div>
   </div>
 </template>
 <script>
