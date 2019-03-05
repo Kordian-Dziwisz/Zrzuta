22a6f34ada235af7d23659f1912d74b16027d0d2
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-03-05 08:43:55 +0100

    Dodanie tekstu na stronie głównej gdy lista jest pusta

--- src/views/Home/list.vue
+++ src/views/Home/list.vue
@@ -1,8 +1,11 @@
 <template>
   <b-container fluid class="w-75">
     <p class="display-4">Lista zbiórek</p>
+    <p
+      class="h5"
+      v-if="list.length === 0"
+    >Brak aktywnych zbiórek - utwórz nową lub poproś o zaproszenie do obecnej.</p>
     <ul class="list-group" v-for="(item, index) in list" :key="index">
-      <p v-if="list.length == 0">Brak aktywnych zbiórek</p>
       <Item class="list-group-item" :item="{index: index, ...item}" @remove="removeItem"/>
     </ul>
   </b-container>
