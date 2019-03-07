5d017775ae2a77f70a2dba5c96fda5e1cf1465ff
Michal Ronkowski <michal.ronkowski@student.witkac.pl>
2019-03-07 08:34:06 +0100

    Zmiana przycisku zmień login na wyloguj, usunięcie komentarzy i css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,23 +7,18 @@
       @shortkey="addFundrais"
     >
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-      <b-navbar-brand href="#" id="paddingCenter">
+      <b-navbar-brand href="#" class="pt-0">
         <img src="@/assets/Logo.jpg" width="50">
       </b-navbar-brand>
-      <!--https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfuNsZHffYNmIn1UcFABUb6-GnknWFFrXIniz1R-Ocer4BMCMug -->
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
-          <b-nav-item :to="{ name: 'home'}">Home</b-nav-item>
+          <b-nav-item :to="{ name: 'home'}">Strona główna</b-nav-item>
           <b-nav-item @click="addFundrais">Nowa zbiórka</b-nav-item>
         </b-navbar-nav>
-
         <b-navbar-nav class="ml-auto">
-          <b-navbar-brand right>
-            <p id="marginCenter">
-              <b-button class="btn-outline-primary btn-light" @click="changeLogin">Zmień login</b-button>
-            </p>
-          </b-navbar-brand>
+          <b-navbar-nav is-nav id="nav-collapse" right>
+            <b-nav-item right @click="changeLogin">Wyloguj</b-nav-item>
+          </b-navbar-nav>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -73,17 +68,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.shadow {
-  box-shadow: 0.5vw 0.5vw 1vh rgba(00, 00, 00, 50%);
-  border-radius: 0.25rem;
-}
-#paddingCenter {
-  padding-top: 0;
-}
-
-#marginCenter {
-  margin: 0;
-}
-</style>
