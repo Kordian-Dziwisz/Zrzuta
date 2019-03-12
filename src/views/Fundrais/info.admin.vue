3519aff5d2c870d63c4067e7daa0c1dc7e439cfb
Kordian <kordian.dziwisz@gmail.com>
2019-03-12 15:47:44 +0100

    Naprawiona aktualizacja informacji o zrzucie i usunięcie console.log

--- src/views/Fundrais/info.admin.vue
+++ src/views/Fundrais/info.admin.vue
@@ -5,81 +5,79 @@
     </b-card-header>
     <b-card>
       <b-card-body v-if="isEdited">
-        <form class="container" @submit.prevent="update">
-          <b-card-title class="mb-0 border-bottom align-middle">
-            <b-row>
-              <b-col>
-                <b-input
-                  class="d-inline"
-                  required
-                  maxlength="50"
-                  placeholder="Tytuł"
-                  v-model.trim="newInfo.title"
-                ></b-input>&nbsp;
-                <!-- <h5 class="font-weight-light d-inline">stworzona przez:&nbsp;</h5>
-                <h5 class="d-inline">{{newInfo.creator}}</h5>-->
-              </b-col>
-              <b-col class="text-right">
-                <h4 class="d-inline text-danger" v-if="newInfo.ended">Zbiórka jest zakończona</h4>
-                <h4 class="d-inline text-success" v-else>Zbiórka jest w trakcie</h4>
-                <b-button
-                  class="mb-2 ml-3 btn-light"
-                  type="button"
-                  :class="{'btn-outline-danger': !newInfo.ended, 'btn-outline-success': newInfo.ended}"
-                  @click="newInfo.ended = !newInfo.ended"
-                  size="sm"
-                >{{newInfo.ended? "Otwórz" : "Zakończ"}}</b-button>&nbsp;
-                <b-button
-                  type="submit"
-                  class="ml-3 mb-2 btn-outline-success btn-light"
-                  size="sm"
-                  @click="isEdited = !isEdited"
-                >Zapisz</b-button>
-              </b-col>
-            </b-row>
-          </b-card-title>
-          <b-card-text>
-            <b-row class="border-bottom py-2">
-              <b-col>
-                <label>Zbiórka :</label>
-                <h5>{{ info.creationDate | moment("LLL")}}</h5>
-              </b-col>
-              <b-col class="text-right">
-                <label>Zbiórka kończy się:</label>
-                <datepicker
-                  v-model="newInfo.endDate"
-                  input-class="form-control bg-white w-50"
-                  :language="pl"
-                  format="D, d MMM yyyy"
-                  :disabledDates="disabledDates"
-                />
-                <timepicker v-model="endTime" format="H:m" @change="updateTime()"/>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col>
-                <label>Opis:</label>&nbsp;
-                <b-textarea
-                  placeholder="Wpisz opis zbiórki"
-                  maxlength="300"
-                  max-rows="10"
-                  v-model="newInfo.description"
-                ></b-textarea>
-              </b-col>
-              <b-col>
-                <label>Informacje o płatności:</label>
-                <b-form-textarea
-                  class="mb-1"
-                  type="text"
-                  maxlength="150"
-                  max-rows="10"
-                  placeholder="Informacje o płatności"
-                  v-model.lazy.trim="newInfo.accountNumber"
-                ></b-form-textarea>
-              </b-col>
-            </b-row>
-          </b-card-text>
-        </form>
+        <b-card-title class="mb-0 border-bottom align-middle">
+          <b-row>
+            <b-col>
+              <b-form-input
+                class="d-inline"
+                required
+                maxlength="50"
+                placeholder="Tytuł"
+                v-model.trim="newInfo.title"
+              ></b-form-input>&nbsp;
+              <!-- <h5 class="font-weight-light d-inline">stworzona przez:&nbsp;</h5>
+              <h5 class="d-inline">{{newInfo.creator}}</h5>-->
+            </b-col>
+            <b-col class="text-right">
+              <h4 class="d-inline text-danger" v-if="newInfo.ended">Zbiórka jest zakończona</h4>
+              <h4 class="d-inline text-success" v-else>Zbiórka jest w trakcie</h4>
+              <b-button
+                class="mb-2 ml-3 btn-light"
+                type="button"
+                :class="{'btn-outline-danger': !newInfo.ended, 'btn-outline-success': newInfo.ended}"
+                @click="newInfo.ended = !newInfo.ended"
+                size="sm"
+              >{{newInfo.ended? "Otwórz" : "Zakończ"}}</b-button>&nbsp;
+              <b-button
+                type="submit"
+                class="ml-3 mb-2 btn-outline-success btn-light"
+                size="sm"
+                @click="update"
+              >Zapisz</b-button>
+            </b-col>
+          </b-row>
+        </b-card-title>
+        <b-card-text>
+          <b-row class="border-bottom py-2">
+            <b-col>
+              <label>Zbiórka :</label>
+              <h5>{{ info.creationDate | moment("LLL")}}</h5>
+            </b-col>
+            <b-col class="text-right">
+              <label>Zbiórka kończy się:</label>
+              <datepicker
+                v-model="newInfo.endDate"
+                input-class="form-control bg-white w-50"
+                :language="pl"
+                format="D, d MMM yyyy"
+                :disabledDates="disabledDates"
+              />
+              <timepicker v-model="endTime" format="H:m" @change="updateTime()"/>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <label>Opis:</label>&nbsp;
+              <b-form-textarea
+                placeholder="Wpisz opis zbiórki"
+                maxlength="300"
+                max-rows="10"
+                v-model="newInfo.description"
+              ></b-form-textarea>
+            </b-col>
+            <b-col>
+              <label>Informacje o płatności:</label>
+              <b-form-textarea
+                class="mb-1"
+                type="text"
+                maxlength="150"
+                max-rows="10"
+                placeholder="Informacje o płatności"
+                v-model.lazy.trim="newInfo.accountNumber"
+              ></b-form-textarea>
+            </b-col>
+          </b-row>
+        </b-card-text>
       </b-card-body>
       <b-card-body class="w-100" v-else>
         <b-card-title class="mb-0 border-bottom align-middle">
@@ -158,7 +156,7 @@ export default {
   },
   methods: {
     update() {
-      console.log("Update");
+      this.isEdited = !this.isEdited;
       this.$emit("info", this.newInfo);
     }
   },
