74fdd0c659e7697891262640bff1e0e34f1a1a3b
Kordian <kordian.dziwisz@gmail.com>
2019-03-20 10:15:25 +0100

    Usunięcie autocomplete z loginh

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,6 +8,7 @@
         type="text"
         maxlength="30"
         :state="validation"
+        autocomplete="off"
       />
       <b-form-invalid-feedback :state="validation">Login musi mieć od 3 do 30 znaków!</b-form-invalid-feedback>
       <b-form-valid-feedback :state="validation"></b-form-valid-feedback>
