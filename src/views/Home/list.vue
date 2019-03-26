25f06314612690898b3015d5c737e2a002977c89
Kordian <kordian.dziwisz@gmail.com>
2019-03-26 14:28:44 +0100

    Dodanie badge

--- src/views/Home/list.vue
+++ src/views/Home/list.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container-fluid w-75">
-    <h2 class="font-weight-light">Lista zbiórek</h2>
-    <h5
-      v-if="list.length === 0"
-    >Brak aktywnych zbiórek - utwórz nową lub poproś o zaproszenie do obecnej.</h5>
+    <h2 class="font-weight-light">
+      <span>Lista zbiórek</span>
+      <b-badge pill variant="success" class="float-right"></b-badge>
+    </h2>
+    <b-alert
+      variant="warning"
+      :show="list.length === 0"
+    >Brak aktywnych zbiórek - utwórz nową lub poproś o zaproszenie do obecnej.</b-alert>
     <ul class="list-group" v-for="(item, index) in list" :key="index">
       <Item class="list-group-item" :item="{index: index, ...item}" @remove="remove"/>
     </ul>
